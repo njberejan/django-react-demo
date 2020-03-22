1bad32423b57ba84df9dd3480f2b94c4de8c84e3
Nick Berejan <nberejan@lexile.com>
2020-03-22 10:56:45 -0400

    added leads.urls to tracking, view created.

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Lead
+from .serializers import LeadSerializer
 
-# Create your views here.
+class LeadListCreate(generics.ListCreateAPIView):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
+
+    
